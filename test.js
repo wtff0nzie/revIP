98a7eb37391923908522f617e45a1b2f77a4faa1
Dez Iddon <dez@backpagecms.net>
2015-04-15 15:38:34 +0100

    Version bump.

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
-getIP = require('./revIP');
+var fetch = require('request'),
+    getIP = require('./revIP');
 
 getIP('93.107.167.81', function (err, data) {
     if (err) {
         return;
     }
 
-    console.log(data);
+    fetch('http://api.openweathermap.org/data/2.5/weather?lat=' + data.geo[0] + '&lon=' + data.geo[1], function (err, res, data) {
+        console.log(data)
+    });
 });
